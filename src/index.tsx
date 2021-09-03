7a094d98b0b46cc68ead4db25cd6c4bf05c7295a
6MIL9 <emilkliavlin2@gmail.com>
2021-09-03 22:51:33 +0500

    learn more about useEffect

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,6 @@ import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-export type UsersType = Array<[number, number]>
-
-const users: UsersType = [
-    [40, 20],
-    [14, 10],
-    [3, 7]
-]
-
-ReactDOM.render(<App users={users} />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 reportWebVitals();
