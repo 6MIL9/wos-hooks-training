7d58bec50d872ac5e80a914d20acf4e3a639cf12
6MIL9 <emilkliavlin2@gmail.com>
2021-09-06 19:55:02 +0500

    add search

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,51 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import s from './App.module.css'
 
+type SearchUserType = {
+  login: string
+  id: number
+}
+
+type SearchResultType = {
+  items: SearchUserType[]
+}
+
 const App = () => {
-  const [selectedUser, setSelectedUser] = useState<string | null>(null)
+  const [selectedUser, setSelectedUser] = useState<SearchUserType | null>(null)
+  const [users, setUsers] = useState<SearchUserType[]>([])
+  const [tempSearch, setTempSearch] = useState<string>('')
+
+  useEffect(() => {
+    if (selectedUser) {
+      document.title = selectedUser.login
+    }
+  }, [selectedUser])
+
+  const fetchData = (term: string) => {
+    axios
+      .get<SearchResultType>(`https://api.github.com/search/users?q=${term}`)
+      .then(res => setUsers(res.data.items))
+  }
+
+  useEffect(() => {
+    fetchData('it-kamasutra')
+  }, [])
+
   return (
     <div className={s.container}>
       <div className={s.item}>
         <div>
-          <input placeholder="search" /> <button>find</button>
+          <input placeholder="search" value={tempSearch} onChange={e => setTempSearch(e.currentTarget.value)} />
+          <button onClick={() => {
+            fetchData(tempSearch)
+          }}>find</button>
         </div>
         <ul>
-          {['Misha', 'Fargo'].map(u => <li className={selectedUser === u ? s.selected : ''} onClick={() => {
+          {users.map(u => <li key={u.id} className={selectedUser === u ? s.selected : ''} onClick={() => {
             setSelectedUser(u)
-            document.title = u
           }}>
-            {u}
+            {u.login}
           </li>)}
         </ul>
       </div>
